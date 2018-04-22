5ca02d69f6915c6dfe340c4680555a3a2db7f922
Pawel Maczka <spunky.pl@gmail.com>
2018-04-22 12:24:54 +0200

    feature/RegExp: refactored storageMiddleware

--- src/storageMiddleware.js
+++ src/storageMiddleware.js
@@ -11,17 +11,7 @@ export function syncViaLocalStorage(action) {
   localStorage.setItem(SYNC_MESSAGE_KEY, uuid());
 }
 
-export function matchesAnyRegExp(text, regExpArray) {
-  const filteredExpressions = regExpArray.filter(element => element instanceof RegExp);
-
-  return filteredExpressions.some(expression => expression.test(text));
-}
-
-export default function createStorageMiddleware({
-  whitelist,
-  blacklist,
-  syncAction = syncViaLocalStorage,
-} = {}) {
+export function validateParams(whitelist, blacklist) {
   if (whitelist && !(whitelist instanceof Array)) {
     throw Error('Whitelist must be an array');
   }
@@ -30,65 +20,51 @@ export default function createStorageMiddleware({
     throw Error('Blacklist must be an array');
   }
 
-  if (whitelist && whitelist.some(element => (
-    !(typeof element === 'string' || element instanceof RegExp)
-  ))) {
+  if (whitelist && whitelist.some(element => (!(typeof element === 'string' || element instanceof RegExp)))) {
     throw new Error('Whitelist array should contain only values of type string or RegExp');
   }
 
-  if (blacklist && blacklist.some(element => (
-    !(typeof element === 'string' || element instanceof RegExp)
-  ))) {
+  if (blacklist && blacklist.some(element => (!(typeof element === 'string' || element instanceof RegExp)))) {
     throw new Error('Blacklist array should contain only values of type string or RegExp');
   }
+}
+
+export const matchesAnyRegExp = (text, regExpArray) => regExpArray.some(element => (
+  element instanceof RegExp && element.test(text)
+));
+
+export default function createStorageMiddleware({
+  whitelist,
+  blacklist,
+  syncAction = syncViaLocalStorage,
+} = {}) {
+  validateParams(whitelist, blacklist);
 
   const whitelistHasRegExp = whitelist && whitelist.some(element => element instanceof RegExp);
   const blacklistHasRegExp = blacklist && blacklist.some(element => element instanceof RegExp);
 
 
   return () => next => action => {
+    const whitelisted = whitelist && (
+      whitelist.includes(action.type) || (whitelistHasRegExp && matchesAnyRegExp(action.type, whitelist))
+    );
+
+    const blacklisted = blacklist && (
+      blacklist.includes(action.type) || (blacklistHasRegExp && matchesAnyRegExp(action.type, blacklist))
+    );
+
     if (!action[IS_REMOTE]) {
       switch (true) {
-        case (
-          !blacklist
-            && whitelist
-            && (whitelist.includes(action.type) || (
-              whitelistHasRegExp && matchesAnyRegExp(action.type, whitelist)
-            ))
-        ): {
+        case (!blacklist && whitelisted):
+        case (blacklist && whitelisted && !blacklisted):
+        case (!whitelist && !blacklisted):
+        case (!whitelist && !blacklist):
           syncAction(action);
           break;
-        }
-        case (
-          blacklist
-            && whitelist
-            && (whitelist.includes(action.type) || (
-              whitelistHasRegExp && matchesAnyRegExp(action.type, whitelist)
-            ))
-            && (!blacklist.includes(action.type) && (
-              !blacklistHasRegExp || !matchesAnyRegExp(action.type, blacklist)
-            ))
-        ): {
-          syncAction(action);
-          break;
-        }
-        case (
-          !whitelist
-            && blacklist
-            && (!blacklist.includes(action.type) && (
-              !blacklistHasRegExp || !matchesAnyRegExp(action.type, blacklist)
-            ))
-        ): {
-          syncAction(action);
-          break;
-        }
-        case (!whitelist && !blacklist): {
-          syncAction(action);
-          break;
-        }
         default: break;
       }
     }
+
     next(action);
   };
 }
